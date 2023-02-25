9f0fdfe27dd31df2e2bfbb598528feac5c785730
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-02-25 10:04:16 +0100

    initialization du projet

---
+++ src/main/java/com/example/khedmabackend/KhedmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.khedmabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhedmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhedmaBackendApplication.class, args);
+	}
+
+}
