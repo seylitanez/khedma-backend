03d0826a9632925f4ef4fbe25fb31e793f81a62d
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2023-03-02 20:30:44 +0100

    add abstact

--- src/main/java/com/example/khedmabackend/KhedmaBackendApplication.java
+++ src/main/java/com/example/khedmabackend/KhedmaBackendApplication.java
@@ -8,7 +8,6 @@ public class KhedmaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KhedmaBackendApplication.class, args);
-
 	}
 
 }
--- src/main/java/com/example/khedmabackend/model/Utilisateur.java
+++ src/main/java/com/example/khedmabackend/model/Utilisateur.java
@@ -2,19 +2,19 @@ package com.example.khedmabackend.model;
 
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
-public class Utilisateur {
+public abstract class Utilisateur {
     @Id
     private String nomUtilisateur;
     private String motDePasse;
+    @Indexed(unique = true)
     private String adresseMail;
     private String nom;
     private String prenom;
     private Genre genre;
     private String tel;
     private Addresse Wilaya;
-    private Utilisateur(){}
-
 }
