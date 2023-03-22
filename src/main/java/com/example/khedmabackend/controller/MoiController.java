49f004b565f87452f05e58b9b4598217b7003cb7
seylitanez <lyes.zenati@se.univ-bejaia.dz>
2023-03-22 19:43:11 +0100

    ajout d'un endpoint qui recupere mes infos de mon profil

---
+++ src/main/java/com/example/khedmabackend/controller/MoiController.java
@@ -0,0 +1,26 @@
+package com.example.khedmabackend.controller;
+
+
+import com.example.khedmabackend.model.Utilisateur;
+import com.example.khedmabackend.services.MeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/me")
+@RequiredArgsConstructor
+public class MoiController {
+    private final MeService meService;
+
+    // renvoi les information de mon profile
+
+    @GetMapping
+    public Utilisateur me(){
+        return meService.me();
+    }
+}
---
+++ src/main/java/com/example/khedmabackend/services/MeService.java
@@ -0,0 +1,23 @@
+package com.example.khedmabackend.services;
+
+import com.example.khedmabackend.model.Utilisateur;
+import com.example.khedmabackend.repo.UtilisateurRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MeService {
+
+    private final UtilisateurRepo utilisateurRepo;
+
+    public Utilisateur me(){
+        String myName=SecurityContextHolder.getContext().getAuthentication().getName();
+        Utilisateur utilisateur= utilisateurRepo.findBynomUtilisateur(myName).orElseThrow();
+
+        return utilisateur;
+
+    }
+
+}
